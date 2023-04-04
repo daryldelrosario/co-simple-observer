4d6977f56cbc3199f058e4c8ca59b2fc5c091829
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-04-04 17:33:15 -0400

    complete intersection observer api function practice

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,34 @@
-alert("hello, world!");
+const boxes = document.querySelectorAll(".box");
+const items = document.querySelectorAll(".item");
+
+const options = {
+    threshold: 0.5
+};
+
+const boxObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        } else {
+            entry.target.classList.remove("visible");
+        }
+    });
+}, options);
+
+const itemObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("slide-in");
+        } else {
+            entry.target.classList.remove("slide-in");
+        }
+    });
+}, { threshold: 1 });
+
+boxes.forEach(box => {
+    boxObserver.observe(box);
+});
+
+items.forEach(item => {
+    itemObserver.observe(item);
+}, options);
